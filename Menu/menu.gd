bae0f5546f9ccb1dc70db477644aeac9073e75fe
raveneer-o1 <yarqwrt@gmail.com>
2025-01-18 22:28:37 +0300

    Improved visuals in the menu.
    Animation will be moved to the main menu. Party constructor will have simpler visuals.

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -2,21 +2,21 @@ extends Control
 
 #const COMMUNICATION_PATH = "res://unit_array.gd"
 
-@onready var position_0: MenuUnitPlace = $"HBoxContainer/VBoxContainer2/VBoxContainer2/Position 0"
-@onready var position_1: MenuUnitPlace = $"HBoxContainer/VBoxContainer2/VBoxContainer/Position 1"
-@onready var position_2: MenuUnitPlace = $"HBoxContainer/VBoxContainer2/VBoxContainer2/Position 2"
-@onready var position_3: MenuUnitPlace = $"HBoxContainer/VBoxContainer2/VBoxContainer/Position 3"
-@onready var position_4: MenuUnitPlace = $"HBoxContainer/VBoxContainer2/VBoxContainer2/Position 4"
-@onready var position_5: MenuUnitPlace = $"HBoxContainer/VBoxContainer2/VBoxContainer/Position 5"
-@onready var position_6: MenuUnitPlace = $"HBoxContainer/VBoxContainer2/VBoxContainer2/Position 6"
+@onready var position_0: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer2/VBoxContainer2/Position 0"
+@onready var position_1: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer2/VBoxContainer/Position 1"
+@onready var position_2: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer2/VBoxContainer2/Position 2"
+@onready var position_3: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer2/VBoxContainer/Position 3"
+@onready var position_4: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer2/VBoxContainer2/Position 4"
+@onready var position_5: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer2/VBoxContainer/Position 5"
+@onready var position_6: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer2/VBoxContainer2/Position 6"
 
-@onready var r_position_0: MenuUnitPlace = $"HBoxContainer/VBoxContainer3/VBoxContainer2/Position 0"
-@onready var r_position_1: MenuUnitPlace = $"HBoxContainer/VBoxContainer3/VBoxContainer/Position 1"
-@onready var r_position_2: MenuUnitPlace = $"HBoxContainer/VBoxContainer3/VBoxContainer2/Position 2"
-@onready var r_position_3: MenuUnitPlace = $"HBoxContainer/VBoxContainer3/VBoxContainer/Position 3"
-@onready var r_position_4: MenuUnitPlace = $"HBoxContainer/VBoxContainer3/VBoxContainer2/Position 4"
-@onready var r_position_5: MenuUnitPlace = $"HBoxContainer/VBoxContainer3/VBoxContainer/Position 5"
-@onready var r_position_6: MenuUnitPlace = $"HBoxContainer/VBoxContainer3/VBoxContainer2/Position 6"
+@onready var r_position_0: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer3/VBoxContainer2/Position 0"
+@onready var r_position_1: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer3/VBoxContainer/Position 1"
+@onready var r_position_2: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer3/VBoxContainer2/Position 2"
+@onready var r_position_3: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer3/VBoxContainer/Position 3"
+@onready var r_position_4: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer3/VBoxContainer2/Position 4"
+@onready var r_position_5: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer3/VBoxContainer/Position 5"
+@onready var r_position_6: MenuUnitPlace = $"MarginContainer/HBoxContainer/VBoxContainer3/VBoxContainer2/Position 6"
 
 
 var right_array : Array[UnitPanel]:
