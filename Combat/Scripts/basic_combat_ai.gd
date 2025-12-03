dd38cc2108b24bddac17ddca19211516a3f401ba
Raveneer-o1 <yarqwrt@gmail.com>
2025-12-03 19:42:13 +0300

    fixed: when AI defends, game crashes with NRE

--- Combat/Scripts/basic_combat_ai.gd
+++ Combat/Scripts/basic_combat_ai.gd
@@ -64,6 +64,7 @@ func choose_action(unit: Unit) -> void:
 	var avaliable_targets := api.combat_system.find_avaliable_targets()
 	if avaliable_targets.is_empty():
 		api.use_defense_stance()
+		return
 	
 	var targets_need := unit.current_attack.targets_needed
 	while targets_need > 0:
