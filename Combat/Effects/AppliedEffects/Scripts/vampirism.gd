cae910feced1a75a22b163053d56ca46a511fd5c
Raveneer-o1 <yarqwrt@gmail.com>
2026-05-10 17:17:07 +0300

    changed Object.call_deferred() to Callable.call_deferred() in the vampirism effect

--- Combat/Effects/AppliedEffects/Scripts/vampirism.gd
+++ Combat/Effects/AppliedEffects/Scripts/vampirism.gd
@@ -21,9 +21,7 @@ func apply_heal(a: Attack)->void:
 
 func vampiric_heal(attack: Attack) ->void:
 	if attack.attacker == target_unit:
-		# NOW: why not apply_heal.call_deferred(attack)?
-		# it should be faster than looking for the function by name
-		self.call_deferred(&"apply_heal", attack)
+		apply_heal.call_deferred(attack)
 
 func read_params(params: Variant) -> void:
 	if params is not Array: return
