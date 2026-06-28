c2d8429c09ef3bde4fe008efc98e7d757ba5f84f
Raveneer-o1 <yarqwrt@gmail.com>
2026-06-28 15:09:46 +0300

    typo fix

--- Combat/Units/Parameters/Policy/Scripts/resurrection_policy.gd
+++ Combat/Units/Parameters/Policy/Scripts/resurrection_policy.gd
@@ -1,6 +1,6 @@
 extends BasePolicy
 
-func apply_policy(attack: Attack, finalize: bool) -> void:
+func _apply_policy(attack: Attack, finalize: bool) -> void:
 	for t in attack.target_references:
 		if not t: continue
 		if not t.spot: continue
