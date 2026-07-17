c767fc150e3b2f3eb4cd1575f433f7453a2952ef
Raveneer-o1 <yarqwrt@gmail.com>
2026-07-17 20:50:22 +0300

    added no immunities intication

--- UI/UnitPanel/immunities_container.gd
+++ UI/UnitPanel/immunities_container.gd
@@ -2,10 +2,20 @@ class_name UI_UnitPanel_ImmunitiesContainer
 extends VBoxContainer
 
 const DAMAGETYPE_TEXTURE_RECT = preload("uid://bstufbu4uucgy")
+const IMMUNITIES_TEXT := "Immunities: "
+const NO_IMMUNITIES_TOOLTIP := "This unit has no immunities"
+
 @onready var h_box_container: HBoxContainer = $HBoxContainer
+@onready var label: Label = $Label
 
 func set_value(a: Array[GlobalDefs.AttackType]) -> void:
 	for c in h_box_container.get_children(): c.queue_free()
+	label.text = IMMUNITIES_TEXT
+	
+	if not a:
+		label.text += "—"
+		tooltip_text = NO_IMMUNITIES_TOOLTIP
+	else: tooltip_text = ""
 	for t in a:
 		var texture: UI_DamageTypeTexture = DAMAGETYPE_TEXTURE_RECT.instantiate()
 		h_box_container.add_child(texture)
