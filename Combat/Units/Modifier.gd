f209f1c4246d1445b6f661788818ccb6ff67a4c5
Raveneer-o1 <yarqwrt@gmail.com>
2026-08-04 19:54:04 +0300

    docs

--- Combat/Units/Modifier.gd
+++ Combat/Units/Modifier.gd
@@ -2,6 +2,8 @@ class_name ModifierStack
 
 var stack: Array[Modifier]
 
+## Returns the value after applying all modifiers in the stack.
+## The [param value] is the underlying value before any modifiers are applied.
 func get_effective_value(value: Variant) -> Variant:
 	if stack.is_empty():
 		return value
@@ -11,6 +13,7 @@ func get_effective_value(value: Variant) -> Variant:
 			last_value = modifier.influence.call(last_value)
 	return last_value
 
+## Removes all modifiers that reference invalid [AppliedEffect] objects.
 func clean() -> void:
 	var stack_copy := stack
 	stack = []
@@ -18,25 +21,28 @@ func clean() -> void:
 		if modifier.valid:
 			stack.append(modifier)
 
+## Adds a modifier associated with the given [param effect] to the stack.
+## If the linked effect is invalidated (e.g., deleted or silenced), the modifier
+## remains in the stack but no longer affects the result value.
+## The modifier uses the provided [param influence] callback to compute the
+## new value for this [ModifierStack]. The callback must accept a single argument
+## (the current value) and return the modified value.
 func add_modifier(effect: AppliedEffect, influence: Callable) -> void:
 	var modifier: Modifier = Modifier.new(effect, influence)
 	stack.append(modifier)
 
 class Modifier:
 	var effect: AppliedEffect
-	
 	var valid: bool:
 		get:
 			return is_instance_valid(effect) and \
 				not effect.is_queued_for_deletion() and \
 				influence.is_valid()
-	
 	var active: bool:
 		get:
 			return valid and \
 				not effect.silenced
 	var influence: Callable
-	
 	func _init(eff: AppliedEffect, infl: Callable) -> void:
 		effect = eff
 		influence = infl
