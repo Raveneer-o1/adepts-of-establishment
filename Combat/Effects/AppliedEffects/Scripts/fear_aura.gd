9ae0672110b9122d5bbff11a8271834d04de10fb
Raveneer-o1 <yarqwrt@gmail.com>
2026-08-07 21:11:43 +0300

    simplified fear aura

--- Combat/Effects/AppliedEffects/Scripts/fear_aura.gd
+++ Combat/Effects/AppliedEffects/Scripts/fear_aura.gd
@@ -4,8 +4,6 @@ extends AuraEffect
 ## Should be less than [code]1.0[/code].
 @export var evasion_decrease: float = 0.5
 
-const DEBUFF_ICON_INDEX = 6
-
 var _added_effects: Dictionary[Unit, AppliedEffect] = {}
 
 func _remove_from(pos: int) -> void:
@@ -61,21 +59,20 @@ func _get_affected_positions(relative_to: int = target_unit.party_position) -> A
 		relative_to + 1,
 		relative_to + 2,
 	]
-	
 	# if unit is in front line, append units of another party as well
-	if Party.is_front_line(relative_to):
-		var adjacent_units_from_another_party := \
-			target_unit.party.other_party.get_units_at_positions(
-				# step of 2 indicates adjacent units in the same row (see Party class documentation)
-				[
-					relative_to, 
-					relative_to - 2, 
-					relative_to + 2
-				], 
-				# don't need nulls for out-of-bounds positions
-				false 
-			)
-		for u in adjacent_units_from_another_party:
-			affected_positions.append( encode_spot(u.spot) )
+	if not Party.is_front_line(relative_to): return affected_positions
 	
+	var adjacent_units := target_unit.party.other_party.get_units_at_positions(
+		# step of 2 indicates adjacent units in the same row (see Party class documentation)
+		[
+			relative_to, 
+			relative_to - 2, 
+			relative_to + 2
+		], 
+		# don't need nulls for out-of-bounds positions
+		false 
+	)
+	
+	for u in adjacent_units:
+		affected_positions.append( encode_spot(u.spot) )
 	return affected_positions
