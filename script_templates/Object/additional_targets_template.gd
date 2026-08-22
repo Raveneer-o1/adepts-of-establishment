8a5daee75d56e1929709e8eff923edc8e93f1204
Raveneer-o1 <yarqwrt@gmail.com>
2026-08-22 15:49:59 +0300

    template update

--- script_templates/Object/additional_targets_template.gd
+++ script_templates/Object/additional_targets_template.gd
@@ -1,4 +1,7 @@
 extends BaseAdditionalTargets
 
+func _max_number_of_targets(attack: UnitAttack) -> int:
+	return 5
+
 func find_additional_targets(attacker: Unit, chosen_targets: Array[UnitSpot]) -> Array[UnitSpot]:
 	return []
